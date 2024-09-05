bf104aa2d7f9e7c3a4d005a819b6c431f71ca487
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-05 13:49:47 +0200

    interface implemented

--- AbstractClassDemo.java
+++ AbstractClassDemo.java
@@ -1,4 +1,25 @@
 
+interface Printable {
+
+    public void print();
+}
+
+class Student implements Printable {
+
+    private String name;
+    private String address;
+
+    Student(String n, String add) {
+        name = n;
+        address = add;
+    }
+
+    public void print() {
+
+        System.out.println("name: " + name + ", address: " + address);
+    }
+}
+
 abstract class Shape {
 
     public abstract void calculateArea();
@@ -29,5 +50,10 @@ public class AbstractClassDemo {
         s.calculateArea();
         Circle s1 = new Circle();
         s1.calculateArea();
+
+        Student s2 = new Student("Shoaib", "Vanloese");
+        s2.print();
+
     }
+
 }
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 <p>if we inherit the class from the abstract class then inherited class has to give implementation of the abstract class</p>
 <p>if inherited class can not give implementation of its parent then the inheritted class will also become abstarct class</p>
 <p>We can not instantiate object from the abstract class, but we can point to the child class due to polymorphism</p>
+
+<h3>Interface</h3>
+<p>Class must provide the implementation of the interface class if it has to use the interface</p>
+<p>implementation means we must give the definition (write methods) in the implemented class. These methods are already signature of the the interface</p>
